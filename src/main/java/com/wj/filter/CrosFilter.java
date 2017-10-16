d139ab28a3a71c0f8aa03320f4c6a0734d4f054d
wd <wangding@handlecar.com>
2017-10-16 12:06:00 +0800

    跨域支持

--- src/main/java/com/wj/filter/CrosFilter.java
+++ src/main/java/com/wj/filter/CrosFilter.java
@@ -20,7 +20,18 @@ public class CrosFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
+        //测试环境用【*】匹配，上生产环境后需要切换为实际的前端请求地址
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+        response.setHeader("Access-Control-Max-Age", "0");
+
+        response.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With");
+
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        response.setHeader("XDomainRequestAllowed","1");
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
